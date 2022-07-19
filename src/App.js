e88c0e2b957987573aa6b59cf3ef398f23f816b6
Tyrone <tyrone.cloma0219@gmail.com>
2022-07-19 17:37:33 -0400

    Added image rendering

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css'
+import { useState } from 'react'
+import MainMenu from './Components/MainMenu';
+import CreateACharacter from './Components/CreateACharacter';
 
 function App() {
-  return (
-    <div className='screen'>
-      <video autoPlay muted loop id="myVideo">
-        <source src="https://a2development.s3.amazonaws.com/H264HD1080.mov" type="video/mp4" />
-      </video>
+const [screen, setScreen] = useState('main')
 
-      <div className='hidden' id='fs-overlay'>
-        <img id='header-img' alt='img' src='https://1000logos.net/wp-content/uploads/2021/08/Dungeons-and-Dragons-Logo-2014.png' />
+    return (
+      <div>
+        {
+          screen === 'main' ? <MainMenu setScreen={setScreen}/>
+          : <CreateACharacter />
+        }
       </div>
-    </div>
-  );
-}
+    );
+  }
 
 export default App;
+    // {
+    //   screen === 'main' ? <MainMenu setScreen={setScreen}/>
+    //   : <CreateACharacter />
+    // }
